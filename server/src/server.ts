d49b0f0a70f9b26d459f0e53d7d8318c253b7586
Flarkinater <mcteaguealex@gmail.com>
2026-08-03 16:51:03 -0400

    Backend file I/O setup

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,12 +2,15 @@
 
 import express from "express";
 import cors from "cors";
+import fs from "fs";
 import path from "path";
 import dotenv from "dotenv";
 
+// Read port from environment variables
 dotenv.config({ path: path.resolve(process.cwd(), "../.env") });
 const port = process.env.BACKEND_PORT || 8080;
 
+// Configure Express and CORS
 const app = express();
 app.use(express.json());
 
@@ -17,6 +20,20 @@ const corsOptions = {  }
 app.use(cors());
 app.use(cors(corsOptions));
 
+// Read/write data to storage file
+const STORAGE_PATH = path.resolve(process.cwd(), "../data/storage.json");
+const readData = () => {
+    if (!fs.existsSync(STORAGE_PATH)) {
+        return [];
+    }
+    const rawData = fs.readFileSync(STORAGE_PATH);
+    return JSON.parse(rawData.toString());
+}
+const writeData = (data: any) => {
+    fs.writeFileSync(STORAGE_PATH, JSON.stringify(data, null, 2));
+}
+
+// Start the server
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
     console.log("Press Ctrl+C to end this process.");
